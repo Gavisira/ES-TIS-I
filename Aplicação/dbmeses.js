d505cc5e5975afc6a46bfee64c61df42292ceb93
Iannarelli <45004842+Iannarelli@users.noreply.github.com>
2019-06-01 10:03:00 -0300

    Inclusão do trabalho finalizado
    Trabalho apresentado na disciplina de TIS I (Eng Software) no 2º semestre de 2018

---
+++ Aplicação/dbmeses.js
@@ -0,0 +1,52 @@
+var meses = {
+    "results": [
+        {
+            "numero": 01,
+            "nome": "Janeiro",
+        },
+        {
+            "numero": 02,
+            "nome": "Fevereiro",
+        },
+        {
+            "numero": 03,
+            "nome": "Março",
+        },
+        {
+            "numero": 04,
+            "nome": "Abril",
+        },
+        {
+            "numero": 05,
+            "nome": "Maio",
+        },
+        {
+            "numero": 06,
+            "nome": "Junho",
+        },
+        {
+            "numero": 07,
+            "nome": "Julho",
+        },
+        {
+            "numero": 08,
+            "nome": "Agosto",
+        },
+        {
+            "numero": 09,
+            "nome": "Setembro",
+        },
+        {
+            "numero": 10,
+            "nome": "Outubro",
+        },
+        {
+            "numero": 11,
+            "nome": "Novembro",
+        },
+        {
+            "numero": 12,
+            "nome": "Dezembro",
+        }
+    ]
+}
